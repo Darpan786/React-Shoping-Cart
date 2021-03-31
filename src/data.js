0301a95e41a2a734f4e8cc5574591074f15ae999
Darpan <darpanagarwal1999@gmail.com>
2021-03-31 11:51:42 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+import App from "./App"
+
+const  data = {
+     products:[
+          {
+               id:"1",
+               name:"Macbook",
+               price:1400,
+               image:"https://picsum.photos/id/180/2400/1600"
+          },
+          {
+               id:"2",
+               name:"Old Car",
+               price:1000,
+               image:"https://picsum.photos/id/120/2400/1600"
+          },
+          {
+               id:"3",
+               name:"Shoes",
+               price:1200,
+               image:"https://picsum.photos/id/130/2400/1600"
+          },
+     ]
+}
+
+export default data;
